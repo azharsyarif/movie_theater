728c7d9f7941a27654f817dd64bc5f799dae70b4
Ahzra <93825303+azharsyarif@users.noreply.github.com>
2023-11-02 14:25:15 +0700

    Create Login Form

--- src/main/java/com/smk/bi/views/helloworld/HelloWorldView.java
+++ src/main/java/com/smk/bi/views/helloworld/HelloWorldView.java
@@ -22,7 +22,7 @@ public class HelloWorldView extends HorizontalLayout {
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+            Notification.show("Halo selamat datang " + name.getValue());
         });
         sayHello.addClickShortcut(Key.ENTER);
 
@@ -30,6 +30,7 @@ public class HelloWorldView extends HorizontalLayout {
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
 
         add(name, sayHello);
+
     }
 
 }
